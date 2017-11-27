f117e70b1e660d2b72170049e49deaaf0ddfec03
Luc Claustres <luc.claustres@orange.fr>
2017-11-27 10:53:51 +0100

    Added block mode to missing examples

--- example/EFHK.js
+++ example/EFHK.js
@@ -3,12 +3,13 @@ const template = require('./jobfile')
 
 const longitude = 24.96333333
 const latitude = 60.31722222
-const resolution = 1000
-const halfWidth = 20000
+const resolution = 30
+const halfWidth = 18000
+const blockResolution = 9000
 
 const job = _.merge({
   id: 'EFHK-' + resolution + '-' + halfWidth,
-  longitude, latitude, resolution, halfWidth
+  longitude, latitude, resolution, halfWidth, blockResolution
 }, template)
 
 module.exports = job
--- example/SPZO.js
+++ example/SPZO.js
@@ -3,12 +3,13 @@ const template = require('./jobfile')
 
 const longitude = -71.93878056
 const latitude = -13.53572222
-const resolution = 500
-const halfWidth = 20000
+const resolution = 30
+const halfWidth = 18000
+const blockResolution = 9000
 
 const job = _.merge({
   id: 'SPZO-' + resolution + '-' + halfWidth,
-  longitude, latitude, resolution, halfWidth
+  longitude, latitude, resolution, halfWidth, blockResolution
 }, template)
 
 module.exports = job
