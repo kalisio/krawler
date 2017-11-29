e3dac60a38617a5b9f43727da2af008c91e8cd26
Christophe Nouguier <christophe.nouguier@free.fr>
2017-11-29 18:21:23 +0100

    Updated WCS server

--- example/jobfile.js
+++ example/jobfile.js
@@ -14,7 +14,7 @@ module.exports = {
     id: '<%= jobId %>-<%= taskId %>.tif',
     type: 'wcs',
     options: {
-      url: 'http://gs.kalisio.xyz/geoserver/Kalisio/wcs',
+      url: 'http://geoserver.kalisio.xyz/geoserver/Kalisio/wcs',
       version: '2.0.1',
       format: 'image/tiff',
       coverageid: 'Kalisio:SRTM',
