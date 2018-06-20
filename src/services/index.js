dc06742782344a430b8ac120e8b90fa4e5adcc11
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-20 12:54:35 +0000

    Deploy kalisio/krawler to github.com/kalisio/krawler.git:gh-pages

--- src/services/index.js
+++
@@ -1,5 +0,0 @@
-import tasks from './tasks'
-import jobs from './jobs'
-import stores from './stores'
-
-export { tasks, jobs, stores }
--- src/stores/index.js
+++
@@ -1,9 +0,0 @@
-import fs from './stores.fs'
-import s3 from './stores.s3'
-import memory from './stores.memory'
-
-export default {
-  fs,
-  s3,
-  memory
-}
