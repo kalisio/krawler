1ef9e65a321cd79d633855b3f8ccc5fb70bd0022
Christophe Nouguier <christophe.nouguier@free.fr>
2018-07-20 10:34:32 +0200

    Updated seeder config

--- examples/seeder/config.js
+++ examples/seeder/config.js
@@ -1,14 +1,12 @@
 module.exports = {
-  seed: {
-    docker: {
-      host: '172-31-41-75'
-    },
-    layer: 'osm-bright',
-		grid: {
-      width: 2,
-      height: 1
-    },
-    levelMin: 0,
-    levelMax: 6
-  }
+  docker: {
+    host: '172-31-41-75'
+  },
+  layer: 'osm-bright',
+  grid: {
+    width: 2,
+    height: 1
+  },
+  levelMin: 0,
+  levelMax: 6
 }
