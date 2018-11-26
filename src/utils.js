5a5d95e37457fa4e354431caa02cae133a4ecdd4
Christophe Nouguier <christophe.nouguier@free.fr>
2018-11-26 12:36:38 +0100

    Fixed #42

--- src/utils.js
+++ src/utils.js
@@ -192,6 +192,8 @@ export function template (item, property) {
       // Add env into templating context
       const context = Object.assign({}, item, process)
       return compiler(context)
+    } else if (typeof value === 'object') {
+      return templateObject(item, value)
     } else {
       return value
     }
