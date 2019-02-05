bcf38d4e5464cf87de155e2090e3b035ddf5d241
Luc Claustres <luc.claustres@orange.fr>
2019-02-05 11:16:38 +0100

    Removed auth from query parameters in overpass task

--- src/tasks/tasks.overpass.js
+++ src/tasks/tasks.overpass.js
@@ -8,7 +8,7 @@ const defaultUrl = 'https://www.overpass-api.de/api/interpreter'
 
 // Build the request parameters to download data from input data source
 function getRequestParameters (options) {
-  let queryParameters = _.merge({}, _.omit(options, ['url', 'headers', 'timeout']))
+  let queryParameters = _.merge({}, _.omit(options, ['url', 'headers', 'timeout', 'auth']))
   // Setup request with URL & params
   debug('Requesting ' + options.url || defaultUrl + ' with following parameters', queryParameters)
   return {
