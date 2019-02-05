2a1c6ad6b27b3c7818c420c8a4fe9d73f0abedbd
Luc Claustres <luc.claustres@orange.fr>
2019-02-05 11:15:17 +0100

    Removed auth from query parameters in HTTP tasks

--- src/tasks/tasks.http.js
+++ src/tasks/tasks.http.js
@@ -6,7 +6,7 @@ const debug = makeDebug('krawler:tasks')
 
 // Build the request parameters to download data from input data source
 function getRequestParameters (options) {
-  let queryParameters = _.merge({}, _.omit(options, ['url', 'headers', 'timeout']))
+  let queryParameters = _.merge({}, _.omit(options, ['url', 'headers', 'timeout', 'auth']))
   // Setup request with URL & params
   debug('Requesting ' + options.url + ' with following parameters', queryParameters)
   return {
