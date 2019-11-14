dabbec840bdbc5808d1481124cedaccf20da25d9
Christophe Nouguier <christophe.nouguier@free.fr>
2019-11-14 11:42:37 +0100

    feat: Enhance travis pipeline (closed #94)

--- .travis.build.sh
+++ .travis.build.sh
@@ -4,8 +4,8 @@
 source .travis.env.sh
 
 # Build the image
-docker build -f dockerfile -t kalisio/krawler:$VERSION .
+docker build -f dockerfile -t $IMAGE_NAME:$IMAGE_TAG .
 
 # Publish the image
 docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
-docker push kalisio/krawler:$VERSION
+docker push $IMAGE_NAME:$IMAGE_TAG
--- .travis.env.sh
+++ .travis.env.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# Build docker with version number only on release
+# Define image name
+IMAGE_NAME="$TRAVIS_REPO_SLUG"
+
+# Define image tag
+# Use version number only on release
 if [[ -z "$TRAVIS_TAG" ]]
 then
-	export VERSION=latest
+	IMAGE_TAG=latest
 else
-	export VERSION=$(node -p -e "require('./package.json').version")
+	IMAGE_TAG=$(node -p -e "require('./package.json').version")
 fi
