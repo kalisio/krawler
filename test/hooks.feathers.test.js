4c1d37aef88b8242780d816a17efdf9db18caa11
Luc Claustres <luc.claustres@orange.fr>
2023-04-13 16:34:53 +0200

    chore: added tests for feathers hooks with a db adapter

--- test/hooks.feathers.test.js
+++ test/hooks.feathers.test.js
@@ -1,31 +1,192 @@
+import _ from 'lodash'
 import chai from 'chai'
 import chailint from 'chai-lint'
 import path, { dirname } from 'path'
 import fs from 'fs-extra'
 import fsStore from 'fs-blob-store'
+import mongo from 'mongodb'
 import { feathers } from '@feathersjs/feathers'
 import socketio from '@feathersjs/socketio'
 import { memory } from '@feathersjs/memory'
+import mongodb from 'feathers-mongodb'
 import { hooks as pluginHooks } from '../lib/index.js'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
+const { MongoClient } = mongo
 const { util, expect } = chai
 
+const inputStore = fsStore({ path: path.join(__dirname, 'data') })
+const geojson = fs.readJsonSync(path.join(inputStore.path, 'geojson.json'))
+
+function createTests (servicePath, feathersHook, options = {}) {
+  it(`creates objects using service ${servicePath}`, async () => {
+    feathersHook.type = 'after'
+    feathersHook.result = feathersHook.data
+    feathersHook.result.data = geojson
+    await pluginHooks.callFeathersServiceMethod({
+      service: servicePath,
+      method: 'create',
+      transform: {
+        omit: ['properties.prop2']
+      }
+    })(feathersHook)
+    const service = feathersHook.data.client.service(servicePath)
+    const results = await service.find({ query: {} })
+    expect(results.length).to.equal(3)
+    expect(results[0].properties).toExist()
+    expect(results[0].properties.prop0).to.equal('value0')
+    expect(results[0].properties.prop2).beUndefined()
+  })
+  // Let enough time to proceed
+    .timeout(5000)
+
+  it(`reads objects using service ${servicePath}`, async () => {
+    await pluginHooks.callFeathersServiceMethod({
+      service: servicePath,
+      method: 'find',
+      query: {
+        $skip: 1,
+        $limit: 2
+      },
+      transform: {
+        omit: ['properties.prop0']
+      }
+    })(feathersHook)
+    const results = feathersHook.result.data
+    expect(results.length).to.equal(2)
+    expect(results[0].id).to.equal(2)
+    expect(results[0].properties).toExist()
+    expect(results[0].properties.prop0).beUndefined()
+    expect(results[0].properties.prop1).to.equal(0)
+    expect(results[1].id).to.equal(3)
+    expect(results[1].properties).toExist()
+    expect(results[0].properties.prop0).beUndefined()
+  })
+  // Let enough time to proceed
+    .timeout(5000)
+
+  it(`updates objects using service ${servicePath} and data as option`, async () => {
+    await pluginHooks.callFeathersServiceMethod({
+      service: servicePath,
+      method: 'patch',
+      data: { properties: 'value1' },
+      id: null,
+      query: {}
+    })(feathersHook)
+    const service = feathersHook.data.client.service(servicePath)
+    const results = await service.find({ query: {} })
+    expect(results.length).to.equal(3)
+    results.forEach(result => {
+      expect(result.properties).to.equal('value1')
+    })
+  })
+  // Let enough time to proceed
+    .timeout(5000)
+
+  it(`updates objects using service ${servicePath} and data as item`, async () => {
+    feathersHook.data.data = { properties: 'value2' }
+    await pluginHooks.callFeathersServiceMethod({
+      service: servicePath,
+      method: 'patch',
+      id: null,
+      query: {}
+    })(feathersHook)
+    const service = feathersHook.data.client.service(servicePath)
+    const results = await service.find({ query: {} })
+    expect(results.length).to.equal(3)
+    results.forEach(result => {
+      expect(result.properties).to.equal('value2')
+    })
+  })
+  // Let enough time to proceed
+    .timeout(5000)
+
+  it(`updates objects using service ${servicePath} and multiple data as item`, async () => {
+    feathersHook.data.data = [{ id: 1, properties: 'value1' }, { id: 2, properties: 'value1' }, { id: 3, properties: 'value1' }]
+    await pluginHooks.callFeathersServiceMethod({
+      service: servicePath,
+      method: 'patch',
+      id: null,
+      query: { id: '<%= id %>' }
+    })(feathersHook)
+    const service = feathersHook.data.client.service(servicePath)
+    const results = await service.find({ query: {} })
+    expect(results.length).to.equal(3)
+    results.forEach(result => {
+      expect(result.properties).to.equal('value1')
+    })
+  })
+  // Let enough time to proceed
+    .timeout(5000)
+
+  it(`deletes objects using service ${servicePath}`, async () => {
+    await pluginHooks.callFeathersServiceMethod({
+      service: servicePath,
+      method: 'remove',
+      id: null,
+      query: {}
+    })(feathersHook)
+    const service = feathersHook.data.client.service(servicePath)
+    const results = await service.find({ query: {} })
+    expect(results.length).to.equal(0)
+  })
+  // Let enough time to proceed
+    .timeout(5000)
+
+  if (options.upsert) {
+    it(`upserts objects using service ${servicePath} and multiple data as item`, async () => {
+      feathersHook.data.data = [{ id: 1, properties: 'value1' }, { id: 2, properties: 'value1' }, { id: 3, properties: 'value1' }]
+      await pluginHooks.callFeathersServiceMethod({
+        service: servicePath,
+        method: 'patch',
+        id: null,
+        query: { id: '<%= id %>', upsert: true }
+      })(feathersHook)
+      const service = feathersHook.data.client.service(servicePath)
+      const results = await service.find({ query: {} })
+      expect(results.length).to.equal(3)
+      results.forEach(result => {
+        expect(result.properties).to.equal('value1')
+      })
+    })
+    // Let enough time to proceed
+      .timeout(5000)
+  }
+
+  it(`deletes objects using service ${servicePath} and query`, async () => {
+    await pluginHooks.callFeathersServiceMethod({
+      service: servicePath,
+      method: 'remove',
+      id: null,
+      query: { properties: 'value1' }
+    })(feathersHook)
+    const service = feathersHook.data.client.service(servicePath)
+    const results = await service.find({ query: {} })
+    expect(results.length).to.equal(0)
+  })
+  // Let enough time to proceed
+    .timeout(5000)
+}
+
 describe('krawler:hooks:feathers', () => {
-  const inputStore = fsStore({ path: path.join(__dirname, 'data') })
-  let app, server, geojson
+  let mongoClient, db, app, server
 
   before(async () => {
     chailint(chai, util)
+    mongoClient = await MongoClient.connect('mongodb://127.0.0.1:27017/krawler-test')
     app = feathers()
       .configure(socketio({ path: '/ws' }))
-      .use('/geojson', memory({
-        multi: true
-      }))
+      .use('geojson-memory', memory({ multi: true }))
+      .use('geojson-mongodb', mongodb({ multi: true, Model: mongoClient.db('krawler-test').collection('geojson') }))
+    // Add required hook to manage upsert
+    app.service('geojson-mongodb').hooks({
+      before: {
+        patch: (hook) => { _.set(hook, 'params.mongodb', { upsert: _.get(hook, 'params.query.upsert', false) }) }
+      }
+    })
     server = await app.listen(4000)
-    geojson = fs.readJsonSync(path.join(inputStore.path, 'geojson.json'))
   })
 
   const feathersOptions = {
@@ -53,119 +214,8 @@ describe('krawler:hooks:feathers', () => {
     expect(result).to.be.equal(feathersHook)
   })
 
-  it('creates objects using service', async () => {
-    feathersHook.type = 'after'
-    feathersHook.result = feathersHook.data
-    feathersHook.result.data = geojson
-    await pluginHooks.callFeathersServiceMethod({
-      service: '/geojson',
-      method: 'create',
-      transform: {
-        omit: ['properties.prop2']
-      }
-    })(feathersHook)
-    const service = feathersHook.data.client.service('/geojson')
-    const results = await service.find({ query: {} })
-    expect(results.length).to.equal(3)
-    expect(results[0].properties).toExist()
-    expect(results[0].properties.prop0).to.equal('value0')
-    expect(results[0].properties.prop2).beUndefined()
-  })
-  // Let enough time to proceed
-    .timeout(5000)
-
-  it('reads objects using service', async () => {
-    await pluginHooks.callFeathersServiceMethod({
-      service: '/geojson',
-      method: 'find',
-      query: {
-        $skip: 1,
-        $limit: 2
-      },
-      transform: {
-        omit: ['properties.prop0']
-      }
-    })(feathersHook)
-    const results = feathersHook.result.data
-    expect(results.length).to.equal(2)
-    expect(results[0].id).to.equal(2)
-    expect(results[0].properties).toExist()
-    expect(results[0].properties.prop0).beUndefined()
-    expect(results[0].properties.prop1).to.equal(0)
-    expect(results[1].id).to.equal(3)
-    expect(results[1].properties).toExist()
-    expect(results[0].properties.prop0).beUndefined()
-  })
-  // Let enough time to proceed
-    .timeout(5000)
-
-  it('updates objects using service and data as option', async () => {
-    await pluginHooks.callFeathersServiceMethod({
-      service: '/geojson',
-      method: 'patch',
-      data: { properties: 'value1' },
-      id: null,
-      query: {}
-    })(feathersHook)
-    const service = feathersHook.data.client.service('/geojson')
-    const results = await service.find({ query: {} })
-    expect(results.length).to.equal(3)
-    results.forEach(result => {
-      expect(result.properties).to.equal('value1')
-    })
-  })
-  // Let enough time to proceed
-    .timeout(5000)
-
-  it('updates objects using service and data as item', async () => {
-    feathersHook.data.data = { properties: 'value2' }
-    await pluginHooks.callFeathersServiceMethod({
-      service: '/geojson',
-      method: 'patch',
-      id: null,
-      query: {}
-    })(feathersHook)
-    const service = feathersHook.data.client.service('/geojson')
-    const results = await service.find({ query: {} })
-    expect(results.length).to.equal(3)
-    results.forEach(result => {
-      expect(result.properties).to.equal('value2')
-    })
-  })
-  // Let enough time to proceed
-    .timeout(5000)
-
-  it('updates objects using service and multiple data as item', async () => {
-    feathersHook.data.data = [{ id: 1, properties: 'value1' }, { id: 2, properties: 'value1' }, { id: 3, properties: 'value1' }]
-    await pluginHooks.callFeathersServiceMethod({
-      service: '/geojson',
-      method: 'patch',
-      id: null,
-      query: { id: '<%= id %>' }
-    })(feathersHook)
-    const service = feathersHook.data.client.service('/geojson')
-    const results = await service.find({ query: {} })
-    expect(results.length).to.equal(3)
-    results.forEach(result => {
-      expect(result.properties).to.equal('value1')
-    })
-  })
-  // Let enough time to proceed
-    .timeout(5000)
-
-  it('deletes objects using service', async () => {
-    await pluginHooks.callFeathersServiceMethod({
-      service: '/geojson',
-      method: 'remove',
-      id: null,
-      query: {}
-    })(feathersHook)
-    const service = feathersHook.data.client.service('/geojson')
-    const results = await service.find({ query: {} })
-    expect(results.length).to.equal(0)
-  })
-  // Let enough time to proceed
-    .timeout(5000)
+  createTests('geojson-memory', feathersHook)
+  createTests('geojson-mongodb', feathersHook, { upsert: true })
 
   it('disconnect from Feathers', async () => {
     // Cleanup
@@ -183,5 +233,6 @@ describe('krawler:hooks:feathers', () => {
   // Cleanup
   after(async () => {
     await server.close()
+    await mongoClient.close()
   })
 })
